24f20980510fe9ac10ddf940e65f23a68fd4cb19
yongsyongs <ysn3830@gmail.com>
2020-10-23 19:50:26 +0900

    Update DataManager.cs

--- NumpySkeletonVisualizer/Assets/Scripts/DataManager.cs
+++ NumpySkeletonVisualizer/Assets/Scripts/DataManager.cs
@@ -2,7 +2,10 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
+using UnityEngine.UI;
 using NumSharp;
+using TMPro;
+using System;
 
 public class DataManager : MonoBehaviour
 {
@@ -10,13 +13,17 @@ public class DataManager : MonoBehaviour
     [SerializeField] private float scaleFactor = 30f;
 
     private NDArray skeletonArray;
+    private NDArray adjMat;
     private List<GameObject> jointList;
-    private List<Transform> jointTransformList;
 
     private int frameCount;
     private int currentFrame = 0;
     private bool is2D = false;
     private bool pause = false;
+    private bool connectingMode = false;
+    private bool isConnecting = false;
+
+    private int start = -1, end = -1;
 
     void Start()
     {
@@ -25,19 +32,109 @@ public class DataManager : MonoBehaviour
 
     void Update()
     {
-        if (!(pause || skeletonArray is null)) 
+        if(!(skeletonArray is null))
         {
-            if (currentFrame >= frameCount) 
-                currentFrame = 0;
-            NDArray arr = skeletonArray[currentFrame++.ToString()];
-            for (int i = 0; i < jointList.Count; i++)
+            if (!pause)
+                NextFrame();
+            if (!(adjMat is null))
+                RenderSkeleton();
+            if (connectingMode)
+                SetConnectionJoints();
+        }
+    }
+
+    void RenderSkeleton()
+    {
+        for (int i = 0; i < adjMat.shape[0]; i++)
+        {
+            for(int j = i; j < adjMat.shape[1]; j++)
             {
-                if (is2D)
-                    jointTransformList[i].position = new Vector3(arr[i, 0], arr[i, 1]);
-                else
-                    jointTransformList[i].position = new Vector3(arr[i, 0], arr[i, 2], arr[i, 1]);
+                if(adjMat[i, j])
+                {
+                    DrawLine(jointList[i].transform.position, jointList[j].transform.position, Color.red);
+                }
             }
         }
+    }
+
+    void NextFrame()
+    {
+        if (currentFrame >= frameCount)
+            currentFrame = 0;
+        NDArray arr = skeletonArray[currentFrame++.ToString()];
+        for (int i = 0; i < jointList.Count; i++)
+        {
+            if (is2D)
+                jointList[i].transform.position = new Vector3(arr[i, 0], arr[i, 1]);
+            else
+                jointList[i].transform.position = new Vector3(arr[i, 0], arr[i, 2], arr[i, 1]);
+        }
+    }
+
+    void DrawLine(Vector3 start, Vector3 end, Color color)
+    {
+        GameObject myLine = new GameObject();
+        myLine.transform.position = start;
+        myLine.AddComponent<LineRenderer>();
+        LineRenderer lr = myLine.GetComponent<LineRenderer>();
+        lr.material = new Material(Shader.Find("Sprites/Default"));
+        lr.startColor = color;
+        lr.endColor = color;
+        lr.startWidth = 0.1f;
+        lr.endWidth = 0.1f;
+        lr.SetPosition(0, start);
+        lr.SetPosition(1, end);
+        Destroy(myLine, 2f * Time.deltaTime);
+    }
+
+    void SetConnectionJoints()
+    {
+        if (isConnecting)
+        {
+            if (Input.GetMouseButtonUp(0))
+            {
+                var target = GetTarget();
+                JointSphere jsc;
+                if (!(target is null) && target.TryGetComponent<JointSphere>(out jsc))
+                {
+                    end = jsc.JointNumber;
+                    Debug.Log("end was set to " + end);
+                    if (end > 0)
+                    {
+                        adjMat[start, end] = true;
+                        adjMat[end, start] = true;
+                    }
+                }
+                isConnecting = false;
+            }
+        }
+        else
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                var target = GetTarget();
+                JointSphere jsc;
+                if(!(target is null) && target.TryGetComponent<JointSphere>(out jsc))
+                {
+                    start = jsc.JointNumber;
+                    Debug.Log("start was set to " + start);
+                    if (start > 0)
+                        isConnecting = true;
+                }
+            }
+        }
+    }
+
+
+    Transform GetTarget()
+    {
+        RaycastHit hit;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        if (Physics.Raycast(ray, out hit))
+            return hit.transform;
+        else
+            return null;
     }
 
     void GenerateJoints()
@@ -45,7 +142,6 @@ public class DataManager : MonoBehaviour
         if(jointList is null)
         {
             jointList = new List<GameObject>();
-            jointTransformList = new List<Transform>();
 
             int[] dims = skeletonArray.shape;
             int jointNum = dims[1];
@@ -57,7 +153,6 @@ public class DataManager : MonoBehaviour
                 jointList.Add(Instantiate(jointPrefab));
                 jointList[i].AddComponent<JointSphere>();
                 jointList[i].GetComponent<JointSphere>().SetJointIndex(i);
-                jointTransformList.Add(jointList[i].transform);
             }
 
             foreach(var g in jointList)
@@ -68,8 +163,31 @@ public class DataManager : MonoBehaviour
             foreach (GameObject g in jointList)
                 Destroy(g);
             jointList = null;
-            jointTransformList = null;
         }
+    }
+
+    public void SwitchConnectMode()
+    {
+        if (connectingMode)
+            EndConnectMode();
+        else
+            StartConnectMode();
+    }
+
+    public void StartConnectMode()
+    {
+        this.Pause();
+        connectingMode = true;
+        Text text = GameObject.Find("Btn_ConnectMode").transform.GetChild(0).GetComponent<Text>();
+        text.text = "Done";
+    }
+
+    public void EndConnectMode()
+    {
+        this.Resume();
+        connectingMode = false;
+        Text text = GameObject.Find("Btn_ConnectMode").transform.GetChild(0).GetComponent<Text>();
+        text.text = "Connect Joints";
     }
 
     public void Pause()
@@ -82,12 +200,32 @@ public class DataManager : MonoBehaviour
         pause = false;
     }
 
+    public void SaveAdjMatrix()
+    {
+        if(!(adjMat is null))
+        {
+            np.save("Assets/Data/adjmat.npy", adjMat);
+        }
+    }
+
+    public void LoadAdjMatrix()
+    {
+        string path = EditorUtility.OpenFilePanel("Open Adjacency Matrix File", "", "npy");
+        if (path.Length != 0)
+        {
+            adjMat = NPTools.LoadNpyFile(path);
+        }
+    }
+
     public void OpenSkeletonFile()
     {
         string path = EditorUtility.OpenFilePanel("Open Skeleton File", "", "npy");
         if(path.Length != 0)
         {
+            // read npy file that has (T, J, 2 or 3) shape.
             skeletonArray = NPTools.LoadNpyFile(path) * scaleFactor;
+            int jdim = skeletonArray.shape[1];
+            adjMat = np.full<bool>(false, new int[] { jdim, jdim });
             GenerateJoints();
         }
     }
