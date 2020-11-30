68cca28435a135e217490f42c18dbc4134627860
yongsyongs <ysn3830@gmail.com>
2020-11-30 20:17:44 +0900

    add annotation object

---
+++ NumpySkeletonVisualizer/Assets/Scripts/Annotation/AnnotationPoint.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnnotationPoint : MonoBehaviour
+{
+    public int annotNumber = -1;
+
+    public void Init(int num, Color color)
+    {
+        SetNumber(num);
+        this.GetComponent<MeshRenderer>().material.color = color;
+    }
+
+    public void SetNumber(int num)
+    {
+        annotNumber = num;
+    }
+}
